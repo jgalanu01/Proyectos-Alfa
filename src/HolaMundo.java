ff33fe0ca1e1168bf8c6bee584cfb91534cce973
jgalanu01 <jgalanu01@educarex.es>
2023-12-01 13:56:30 +0100

    nuevo

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -3,9 +3,8 @@ public class HolaMundo {
 	public static void main (String[] args) {
 		
 		
-		System.out.println("Hola mundo");
+		System.out.println("Hola putos");
 		System.out.println("Buenos dias");
-
 	
 	
 	
